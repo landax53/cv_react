b127474c1339b8e91bdfd909295ea2fa6b3a8316
Landax <vincentlandais@aol.fr>
2021-09-15 14:01:11 +0200

    correctifs media queries mobile

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,55 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Project extends Component {
-    state = {
-        showInfo: false
-    }
+  state = {
+    showInfo: false,
+  };
 
-    handleInfo = () => {  /* fonction qui permet de rendre vrai ou faux showinfo */
-        this.setState({
-            showInfo:!this.state.showInfo
-        })
-    }
+  handleInfo = () => {
+    /* fonction qui permet de rendre vrai ou faux showinfo */
+    this.setState({
+      showInfo: !this.state.showInfo,
+    });
+  };
 
-    render() {
-        let {name, languagesIcons, source, info, picture, backend} = this.props.item;
-        return (
-            <div className="project">
-                {/*Carte*/}
-                <div className="icons">
-                    {languagesIcons.map(icon => 
-                        <i className={icon} key={icon}></i>
-                    )}
+  render() {
+    let { name, languagesIcons, source, info, picture } = this.props.item;
+    return (
+      <div className="project">
+        {/*Carte*/}
+        <div className="icons">
+          {languagesIcons.map((icon) => (
+            <i className={icon} key={icon}></i>
+          ))}
+        </div>
+        <h3>{name}</h3>
+        <img src={picture} alt="" onClick={this.handleInfo} />
+        {/* <h5>Base de données : {backend}</h5> */}
+        <span className="infos" onClick={this.handleInfo}>
+          <i className="fas fa-plus-circle"></i>
+        </span>
+        {this.state.showInfo /* si showInfo est vrai et ...*/ && (
+          <div className="showInfos">
+            <div className="infosContent">
+              <div className="head">
+                <h2>{name}</h2>
+                <div className="sourceCode">
+                  <a
+                    href={source}
+                    rel="noopener noreferrer"
+                    className="button"
+                    target="_blank"
+                  >
+                    Code Source
+                  </a>
                 </div>
-                <h3>{name}</h3>
-                <img src={picture} alt="" onClick={this.handleInfo}/>
-                {/* <h5>Base de données : {backend}</h5> */}
-                <span className="infos" onClick={this.handleInfo}>
-                    <i className="fas fa-plus-circle"></i>
-                </span>
-                {
-                    this.state.showInfo && ( /* si showInfo est vrai et ...*/
-                        <div className="showInfos">
-                            <div className="infosContent">
-                                <div className="head">
-                                    <h2>{name}</h2>
-                                    <div className="sourceCode">
-                                        <a href={source} rel='noopener noreferrer' 
-                                        className="button" target="_blank"
-                                        >Code Source</a>
-                                    </div>
-                                </div>
+              </div>
 
-                                <p className="text">{info}</p>
+              <p className="text">{info}</p>
 
-                                <div className="button return" onClick={this.handleInfo}>Retourner sur la page</div> 
-                            </div>
-                        </div>
-                    )
-                }
+              <div className="button return" onClick={this.handleInfo}>
+                Retourner sur la page
+              </div>
             </div>
-        );
-    }
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Project;
